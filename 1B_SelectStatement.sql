c0306b76a870b55661bd83a4a903312f4741b166
Vanshshah55 <vips.vansh@gmail.com>
2025-10-24 20:08:55 +0530

    Basic Select Statement with concat

---
+++ 1B_SelectStatement.sql
@@ -0,0 +1,14 @@
+Select * from Employees
+
+select firstname from employees
+
+select firstname,salary from employees
+
+select firstname, lastname from employees
+
+select employeeid,concat(firstname,' ',lastname) from Employees
+
+select employeeid,concat(firstname,' ',lastname) [Full name] from Employees
+
+select employeeid, concat(firstname,' ', lastname) as [Full Name]
+from employees
