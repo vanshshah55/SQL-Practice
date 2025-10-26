ed1aee50b475e46bd23bba55a0e4a497aee326a8
Vanshshah55 <vips.vansh@gmail.com>
2025-10-26 22:33:06 +0530

    Table Creation for Joins

---
+++ 19A_CreateTable.sql
@@ -0,0 +1,21 @@
+use sales
+
+create table table1 (C1 int, C2 nvarchar(max))
+
+insert into table1 values (1,'A'),
+(1,'B'),
+(2,'C'),
+(NULL,'D'),
+(3,'E'),
+(7,'DA')
+
+create table table2 (C1 int, C3 nvarchar(max))
+
+insert into table2 values (1,'XA'),
+(2,'MB'),
+(2,'NX'),
+(NULL,'MO'),
+(4,'XY'),
+(5,'TF')
+
+
