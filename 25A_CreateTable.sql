81e872aad0ba0327b64b8b19f61d96ae9197bebf
Vanshshah55 <vips.vansh@gmail.com>
2025-10-28 00:40:12 +0530

    Table Creation for UNION & UNION ALL Operator

---
+++ 25A_CreateTable.sql
@@ -0,0 +1,19 @@
+
+create table append1 
+(C1 int,C2 nvarchar(255),C3 int)
+
+insert into append1 
+values 
+(1,'A',7),
+(2,'B',8),
+(3,'C',9)
+
+
+create table append2 
+(C1 int,C2 nvarchar(255),C3 int)
+
+insert into append1 
+values 
+(11,'AA',17),
+(2,'B',8),
+(33,'C1',91)
