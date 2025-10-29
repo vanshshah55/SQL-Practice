60306488fb3f426b69b0aa237a80b93ffe2da2f7
Vanshshah55 <vips.vansh@gmail.com>
2025-10-30 00:00:37 +0530

    Inserting Records into Certain Columns in a Table

--- 32_InsertRecordsInCetainColumns.sql
+++ 32_InsertRecordsInCetainColumns.sql
@@ -8,3 +8,9 @@ select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME like 'student_details'
 --Datatype of column age is tinyint (0 to 255)
 insert into student_details (Age)
 values (256) -- will not work
+
+
+--You can insert records into specific columns of a table by specifying the column names in the INSERT statement.
+--If you insert values only for some columns, the remaining columns will have NULL values unless a default is specified.
+--It is important to know the data type and allowed range for each column to avoid errors when inserting data.
+--Attempting to insert a value outside the allowed range for a column's data type (such as 256 into a TINYINT column) will result in an error.
