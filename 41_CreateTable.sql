c5fb6fbcf0cebb1954917e4f25d99ff8084f902a
Vanshshah55 <vips.vansh@gmail.com>
2025-10-30 03:15:02 +0530

    Table Creation for SQL Order of Execution

---
+++ 41_CreateTable.sql
@@ -0,0 +1,20 @@
+use employee_details
+
+CREATE TABLE EmployeeSalaries (
+    EmployeeID INT,
+    EmployeeName VARCHAR(50),
+    Salary INT,
+    Department VARCHAR(50)
+);
+
+INSERT INTO EmployeeSalaries (EmployeeID, EmployeeName, Salary, Department)
+VALUES
+(1, 'Alice', 50000, 'HR'),
+(2, 'Bob', 60000, 'HR'),
+(3, 'Charlie', 55000, 'HR'),
+(4, 'David', 75000, 'Finance'),
+(5, 'Eve', 80000, 'Finance'),
+(6, 'Frank', 72000, 'Finance'),
+(7, 'Grace', 90000, 'IT'),
+(8, 'Heidi', 95000, 'IT'),
+(9, 'Ivan', 87000, 'IT');
