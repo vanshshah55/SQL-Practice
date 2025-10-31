db8014438178babe21f39369ef53a3a549e7ea8f
Vanshshah55 <vips.vansh@gmail.com>
2025-10-31 16:07:35 +0530

    Table Creation for Window Functions

---
+++ 42A_CreateTable.sql
@@ -0,0 +1,44 @@
+use student
+
+CREATE TABLE Students (
+    student_name VARCHAR(100),
+    subject VARCHAR(100),
+    marks INT
+);
+
+INSERT INTO Students (student_name, subject, marks)
+VALUES 
+-- Marks for Alice
+('Alice', 'Math', 85),
+('Alice', 'Science', 88),
+('Alice', 'English', 92),
+
+-- Marks for Bob
+('Bob', 'Math', 90),
+('Bob', 'Science', 78),
+('Bob', 'English', 85),
+
+-- Marks for Charlie
+('Charlie', 'Math', 85),
+('Charlie', 'Science', 82),
+('Charlie', 'English', 80),
+
+-- Marks for David
+('David', 'Math', 92),
+('David', 'Science', 91),
+('David', 'English', 89),
+
+-- Marks for Eve
+('Eve', 'Math', 90),
+('Eve', 'Science', 85),
+('Eve', 'English', 87),
+
+-- Marks for Frank
+('Frank', 'Math', 75),
+('Frank', 'Science', 72),
+('Frank', 'English', 78),
+
+-- Marks for Grace
+('Grace', 'Math', 85),
+('Grace', 'Science', 89),
+('Grace', 'English', 90);
