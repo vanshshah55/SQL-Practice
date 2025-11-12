c6d454a7b73fb8437617730000da0ab0b14da5e3
Vanshshah55 <vips.vansh@gmail.com>
2025-11-12 17:13:52 +0530

    Basic Joins - SQL 50 Question 12

--- LEETCODE SQL 50 Questions/12_1280-Students and Examinations.sql
+++ LEETCODE SQL 50 Questions/12_1280-Students and Examinations.sql
@@ -135,3 +135,4 @@ Alice attended the Math exam 3 times, the Physics exam 2 times, and the Programm
 Bob attended the Math exam 1 time, the Programming exam 1 time, and did not attend the Physics exam.
 Alex did not attend any exams.
 John attended the Math exam 1 time, the Physics exam 1 time, and the Programming exam 1 time.
+*/
