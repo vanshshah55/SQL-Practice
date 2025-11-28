524881951faf18e124f3c62b2ee3a8f05db01e93
Vanshshah55 <vips.vansh@gmail.com>
2025-11-29 00:03:52 +0530

    Advanced Select and Joins - SQL 50 Question 33

---
+++ LEETCODE SQL 50 Questions/33_180-Consecutive Numbers.sql
@@ -0,0 +1,60 @@
+
+with cte as (
+select *, lead(num) over (order by id) [leadd], 
+lag(num) over (order by id) [lagg]
+from logs
+)
+select distinct num [ConsecutiveNums] from cte
+where num = leadd and num = lagg
+
+
+/*
+QUESTION : -
+180. Consecutive Numbers
+
+Medium
+Topics
+
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column starting from 1.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+*/
